e9508c532aae3d92b526abda1230bdf1e8d71b83
Marcel Žec <zecmarce@fel.cvut.cz>
2020-03-23 21:41:49 +0100

    frondend

---
+++ FrontEnd/travel-and-work-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Aplikacia hotova. Môžeme odovzdavať!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
