cbacba6cb1e667c6f61e601f54c304881af68931
Marcel Žec <zecmarce@fel.cvut.cz>
2020-03-23 13:47:31 +0100

    edit of trip and user controller with comments for Anicka

--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/rest/TripController.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/rest/TripController.java
@@ -1,9 +1,11 @@
 package cz.cvut.fel.rsp.travelandwork.rest;
 
 import cz.cvut.fel.rsp.travelandwork.exception.NotFoundException;
+import cz.cvut.fel.rsp.travelandwork.model.Trip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -32,7 +34,7 @@ public class TripController {
 
     }
 
-    @PostMapping(value = "/{identificator}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/{identificator}")
     public void update(@PathVariable String identifictor) throws NotFoundException{
 
     }
@@ -40,6 +42,32 @@ public class TripController {
     @DeleteMapping(value = "/{identificator}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String identifictor) throws NotFoundException {
+
+    }
+
+    @PostMapping(value = "/{identificator}")
+    public ResponseEntity<Void> signUpToTrip(@RequestBody Trip trip) {
+        //return new ResponseEntity<>(headers, HttpStatus.SUCCESS);
+        return null;
+    }
+
+    @GetMapping(value = "/trips", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void showAllActiveTripsOfUser() {
+
+    }
+
+    @GetMapping(value = "/history", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void showTripHistoryOfUser() {
+
+    }
+
+    @GetMapping(value = "/cannotAfford", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void showAllTripsCantUserAfford() {
+
+    }
+
+    @GetMapping(value = "/canAfford", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void showAllTripsCanUserAfford() {
 
     }
 }
--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/rest/UserController.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/rest/UserController.java
@@ -34,41 +34,45 @@ public class UserController {
 
 
 
-    @GetMapping(value = "/trips", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void showAllTrips(@RequestBody User user) {
+    //TODO - od Marcela - presunute do TripControlerra a odstranený requestbody lebo to vieme zistit
+//    @GetMapping(value = "/trips", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public void showAllTrips(@RequestBody User user) {
+//
+//    }
+
+    //TODO - od Marcela - to je otázka či to budeme mať jak osobitné dotazy alebo to budeme filtrovať na frontende, ale ak to bude jak dotaz tak som to dal do TripControllera
+//    @GetMapping(value = "/tripsCantAfford", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public void showAllTripsCantAfford(@RequestBody User user) {
+//
+//    }
+//
+//    @GetMapping(value = "/tripsCanAfford", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public void showAllTripsCanAfford(@RequestBody User user) {
+//
+//    }
+
+    //TODO - od Marcela - tak ako sme pisali na Slacku, že to asi bude cez dotaz na trip jurnal
+//    @GetMapping(value = "/trips", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public void showTripHistory(@RequestBody User user) {
+//
+//    }
+
+    //TODO - od Marcela - len som dal preč requestbody lebo vieme zistit kto je prihlaseny
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public void showCurrentUser() {
 
     }
 
-    @GetMapping(value = "/tripsCantAfford", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void showAllTripsCantAfford(@RequestBody User user) {
+    //TODO - od Marcela - nato môžeme používať SettingController z našej semestrálky z EAR
+//    //Upravit si svuj profil - heslo
+//    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Void> changePassword(@RequestBody User user) {
+//        return null;
+//    }
 
-    }
-
-    @GetMapping(value = "/tripsCanAfford", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void showAllTripsCanAfford(@RequestBody User user) {
-
-    }
-
-
-    @GetMapping(value = "/trips", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void showTripHistory(@RequestBody User user) {
-
-    }
-
-    @GetMapping(value = "/currentUser", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void showUserDetails(@RequestBody User user) {
-
-    }
-
-    //Upravit si svuj profil - heslo
+    //TODO - od Marcela - asi je zbytočné mať metoódu len na zmenu emailu, radšej na celý profil, tak som to len prepísal z changeEmail na update
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> changePassword(@RequestBody User user) {
-        return null;
-    }
-
-    //Upravit si svuj profil - email
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> changeEmail(@RequestBody User user) {
+    public ResponseEntity<Void> update(@RequestBody User user) {
         return null;
     }
 
