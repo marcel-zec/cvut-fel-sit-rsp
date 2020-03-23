0b6be75f1b204f63229fec50aa4d97c640140920
Marcel Žec <zecmarce@fel.cvut.cz>
2020-03-23 22:30:51 +0100

    routes for user

--- FrontEnd/travel-and-work-app/src/App.js
+++ FrontEnd/travel-and-work-app/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//na routovanie v aplikacii
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+    //obalia sa vsetky komponenty, ktore maju zvladat routovanie
+    <Router>
+        <Switch>   
+                {/*Global*/}
+                <Route path="/login" exact component={Login} />
+                <Route path="/logout" exact component={Logout} />
+                {/*User*/}
+                <Route path="/register" exact component={Register} />
+                <Route path="/" exact component={Home} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/trips" component={TripList} />
+                <Route path="/trips/:id" component={Trip} />
+                <Route path="/trips/history" component={TripHistoryList} />
+                <Route path="/trips/history/:id" component={TripHistory} />
+        </Switch>    
+
     <div className="App">
       <header className="App-header">
         <h1>Ahojky</h1>
@@ -13,6 +31,8 @@ function App() {
         </p>
       </header>
     </div>
+
+    </Router>
   );
 }
 
