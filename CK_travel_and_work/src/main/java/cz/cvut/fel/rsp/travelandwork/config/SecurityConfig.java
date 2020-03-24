080d9e0aa9ec9d9a2fa2078f19c4dcc642f8dcea
Marcel Žec <zecmarce@fel.cvut.cz>
2020-03-24 21:02:07 +0100

    commit before rebase

--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/config/SecurityConfig.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/config/SecurityConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
