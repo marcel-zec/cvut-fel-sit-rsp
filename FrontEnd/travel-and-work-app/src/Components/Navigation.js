00e7adf5e4820e4b0d2b3c75829be67f450a1d12
Marcel Žec <zecmarce@fel.cvut.cz>
2020-03-25 13:05:09 +0100

    navigation

--- FrontEnd/travel-and-work-app/src/Components/Navigation.js
+++ FrontEnd/travel-and-work-app/src/Components/Navigation.js
@@ -6,47 +6,78 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faTrophy,
+    faPowerOff,
+    faCog,
+    faSuitcase
+} from "@fortawesome/free-solid-svg-icons";
 
 class Navigation extends React.Component {
     render() {
         return (
             <Container>
                 <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Col>
+                        <Navbar.Brand href="#home">
+                            React-Bootstrap
+                        </Navbar.Brand>
+                    </Col>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown
-                                title="Dropdown"
-                                id="basic-nav-dropdown"
-                            >
-                                <NavDropdown.Item href="#action/3.1">
-                                    Action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">
-                                    Something
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Form inline>
-                            <FormControl
-                                type="text"
-                                placeholder="Search"
-                                className="mr-sm-2"
-                            />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+                        <Col xs={9}>
+                            <Form inline>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Search"
+                                    className="mr-sm-2"
+                                />
+                                <Button variant="outline-success">
+                                    Search
+                                </Button>
+                            </Form>
+                        </Col>
+                        <Col>
+                            <Nav className="mr-auto">
+                                <Col>
+                                    <Nav.Link href="#home">Home</Nav.Link>
+                                </Col>
+                                <Col>
+                                    <NavDropdown
+                                        title=""
+                                        id="basic-nav-dropdown"
+                                    >
+                                        <NavDropdown.Item>XP</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="#action/3.2">
+                                            My achievments{" "}
+                                            <FontAwesomeIcon icon={faTrophy} />
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2">
+                                            My trips
+                                            <FontAwesomeIcon
+                                                icon={faSuitcase}
+                                            />
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.3">
+                                            Settings
+                                            <FontAwesomeIcon icon={faCog} />
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.4">
+                                            Log out
+                                            <FontAwesomeIcon
+                                                icon={faPowerOff}
+                                            />
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </Col>
+                            </Nav>
+                        </Col>
                     </Navbar.Collapse>
                 </Navbar>
+                <Row></Row>
             </Container>
         );
     }
