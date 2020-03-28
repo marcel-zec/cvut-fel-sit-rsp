f38f73a3167eb5fc8c6e3ce90ae8d99970515e24
Sandra Hamráková <hamrasan@fel.cvut.cz>
2020-03-28 15:21:55 +0100

    delete, update, findbystring  trip service
     trip controller - logger, update,

--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/service/TripService.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/service/TripService.java
@@ -1,6 +1,7 @@
 package cz.cvut.fel.rsp.travelandwork.service;
 
 import cz.cvut.fel.rsp.travelandwork.dao.TripDao;
+import cz.cvut.fel.rsp.travelandwork.exception.NotFoundException;
 import cz.cvut.fel.rsp.travelandwork.model.Trip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,4 +34,45 @@ public class TripService {
     public Trip find(Long id) {
         return tripDao.find(id);
     }
+
+    @Transactional
+    public Trip findByString(String stringId) {
+        return tripDao.find(stringId);
+    }
+
+    @Transactional
+    public Trip findAfford(Long id) {
+        return tripDao.find(id);
+    }
+
+    @Transactional
+    public Trip findNotAfford(Long id) {
+        return tripDao.find(id);
+    }
+
+    @Transactional
+    public void update(String stringId, Trip newTrip) throws NotFoundException {
+        Trip trip = tripDao.find(stringId);
+
+        if (trip == null) throw new NotFoundException();
+        //pridat vynimku na rolu
+
+        trip.setDescription(newTrip.getDescription());
+        trip.setName(newTrip.getName());
+        trip.setPhone_number(newTrip.getPhone_number());
+        trip.setPossible_xp_reward(newTrip.getPossible_xp_reward());
+        trip.setRating(newTrip.getRating());
+        tripDao.update(trip);
+    }
+
+    @Transactional
+    public void delete(String stringId) throws NotFoundException {
+
+        Trip trip = tripDao.find(stringId);
+        if (trip == null) throw new NotFoundException();
+        // urobit priznak
+    }
+
+
+
 }
