0c0d5eadb99ad66805fb5a3cb4bb61f34b3a0511
Sandra Hamráková <hamrasan@fel.cvut.cz>
2020-03-28 19:54:49 +0100

    trip pridanie atributov achievement, location, req. level, deposit

--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/model/Trip.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/model/Trip.java
@@ -106,14 +106,6 @@ public class Trip extends AbstractEntity {
         this.description = description;
     }
 
-    public String getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
     public double getRating() {
         return rating;
     }
