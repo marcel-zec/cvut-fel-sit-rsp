c226b33c29b2cfddd8562150b24997affb205ce6
Marcel Žec <zecmarce@fel.cvut.cz>
2020-04-01 00:19:58 +0200

    create trip form

--- FrontEnd/travel-and-work-app/src/Components/Admin/Trip/Create.js
+++ FrontEnd/travel-and-work-app/src/Components/Admin/Trip/Create.js
@@ -1,13 +1,84 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import { Col, Button } from "react-bootstrap";
+import { Col, Button, Row } from "react-bootstrap";
 import { Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class ProfileDetails extends React.Component {
+    state = { achievements: null, show: true, categories: null };
+
+    fetchAchievementsHandler = async () => {
+        const response = await fetch(`http://localhost:8080/achievement`);
+        const data = await response.json();
+        console.log(data);
+        //show: false -> add class name to button and hide it
+        this.setState({ achievements: data, show: false });
+    };
+
+    async componentDidMount() {
+        const response = await fetch(`http://localhost:8080/category`);
+        const data = await response.json();
+        console.log(data);
+        this.setState({ categories: data });
+    }
+
     render() {
+        let categoryOptions = null;
+        if (this.state.categories !== null) {
+            if (this.state.categories.lenght > 0) {
+                let categoriesArray = [];
+                this.state.achievements.forEach(element => {
+                    categoriesArray.push(<option>{element.name}</option>);
+                });
+                categoryOptions = (
+                    <Form.Control as="select">{categoriesArray}</Form.Control>
+                );
+            }
+        }
+
+        let requiredAchievements = null;
+        let gainAchievements = null;
+        if (this.state.achievements !== null) {
+            if (this.state.achievements.length > 0) {
+                let achievementsArray = [];
+                this.state.achievements.forEach(element => {
+                    let achievementElement = (
+                        <>
+                            <Form.Check.Label>
+                                <Form.Check.Input type="checkbox" />
+                                <FontAwesomeIcon
+                                    icon={element.icon}
+                                    size="lg"
+                                />
+                                {element.name}
+                            </Form.Check.Label>
+                        </>
+                    );
+                    achievementsArray.push(achievementElement);
+                });
+                requiredAchievements = (
+                    <Form.Group>
+                        <Form.Label>Required achievements</Form.Label>
+                        <div className="d-flex flex-column align-items-start ml-5">
+                            {achievementsArray}
+                        </div>
+                    </Form.Group>
+                );
+                gainAchievements = (
+                    <Form.Group>
+                        <Form.Label>Gain achievements</Form.Label>
+                        <div className="d-flex flex-column align-items-start ml-5">
+                            {achievementsArray}
+                        </div>
+                    </Form.Group>
+                );
+            }
+        }
+
         return (
             <Container>
-                <Form className="mt-5">
+                <Form className="mt-3 mb-5">
+                    <h1>Create trip</h1>
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridName">
                             <Form.Label>Name of trip</Form.Label>
@@ -19,7 +90,6 @@ class ProfileDetails extends React.Component {
                             <Form.Control placeholder="Enter unique key for trip" />
                         </Form.Group>
                     </Form.Row>
-
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridDeposit">
                             <Form.Label>Deposit</Form.Label>
@@ -31,17 +101,60 @@ class ProfileDetails extends React.Component {
                             <Form.Control placeholder="Enter minimum reqiured level" />
                         </Form.Group>
                     </Form.Row>
+                    <Form.Row>
+                        <Form.Group
+                            as={Col}
+                            controlId="exampleForm.ControlSelect1"
+                        >
+                            <Form.Label>Possible XP reward</Form.Label>
+                            <Form.Control as="select">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Form.Control>
+                        </Form.Group>
 
+                        <Form.Group
+                            as={Col}
+                            controlId="exampleForm.ControlSelect1"
+                        >
+                            <Form.Label>Category</Form.Label>
+                        </Form.Group>
+                    </Form.Row>
                     <Form.Group controlId="formGridLocation">
                         <Form.Label>Location</Form.Label>
                         <Form.Control placeholder="Hotel Super, London Street 12, Manchester, England" />
                     </Form.Group>
-
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Description</Form.Label>
                         <Form.Control as="textarea" rows="5" />
                     </Form.Group>
-
+                    <Button
+                        variant="primary"
+                        type="button"
+                        onClick={() => this.fetchAchievementsHandler()}
+                        className={this.state.show ? "" : "d-none"}
+                    >
+                        Achievements
+                    </Button>
+                    <Form.Row>
+                        <Form.Group
+                            as={Col}
+                            className="pl-5"
+                            controlId="formGridName"
+                        >
+                            {requiredAchievements}
+                        </Form.Group>
+                        <Form.Group
+                            as={Col}
+                            className="pl-5"
+                            controlId="formGridName"
+                        >
+                            {gainAchievements}
+                        </Form.Group>
+                    </Form.Row>
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
