d2aa72c1d6fce8e7717198de3ca5bfbe9c6dc616
Marcel Žec <zecmarce@fel.cvut.cz>
2020-03-31 15:55:33 +0200

    spinner to home when no answer from backend

--- FrontEnd/travel-and-work-app/src/Components/Home/Home.js
+++ FrontEnd/travel-and-work-app/src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import TripSmall from "./TripSmall";
 import CardColumns from "react-bootstrap/CardColumns";
+import Spinner from "react-bootstrap/Spinner";
 
 class Home extends React.Component {
     async componentDidMount() {
@@ -10,18 +11,30 @@ class Home extends React.Component {
         console.log(data);
         this.setState({ trips: data });
     }
-    state = { trips: [] };
+    state = { trips: null };
 
     render() {
-        return (
-            <Container>
-                <CardColumns>
-                    {this.state.trips.map(trip => {
-                        return <TripSmall key={trip.short_name} trip={trip} />;
-                    })}
-                </CardColumns>
-            </Container>
-        );
+        if (this.state.trips === null) {
+            return (
+                <Container className="p-5">
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                </Container>
+            );
+        } else {
+            return (
+                <Container>
+                    <CardColumns>
+                        {this.state.trips.map(trip => {
+                            return (
+                                <TripSmall key={trip.short_name} trip={trip} />
+                            );
+                        })}
+                    </CardColumns>
+                </Container>
+            );
+        }
     }
 }
 
