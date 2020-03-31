e289d5ef6cdfdce2ca23e700eee13b220501928b
Sandra Hamráková <hamrasan@fel.cvut.cz>
2020-03-31 15:32:43 +0200

    tripsessiondao find metoda tripsession namedquery tripsessioncontroller findallintrip metoda tripsessionservice findallintrip metoda, constructor

--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/dao/TripSessionDao.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/dao/TripSessionDao.java
@@ -3,9 +3,23 @@ package cz.cvut.fel.rsp.travelandwork.dao;
 import cz.cvut.fel.rsp.travelandwork.model.TripSession;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
+import java.util.List;
+
 @Repository
 public class TripSessionDao extends BaseDao<TripSession> {
     public TripSessionDao() {
         super(TripSession.class);
+    }
+
+    public List<TripSession> find(String trip_short_name){
+        {
+            try {
+                return em.createNamedQuery("TripSession.findByTrip", TripSession.class).setParameter("trip_short_name", trip_short_name)
+                        .getResultList();
+            } catch (NoResultException e) {
+                return null;
+            }
+        }
     }
 }
--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/service/TripSessionService.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/service/TripSessionService.java
@@ -1,7 +1,26 @@
 package cz.cvut.fel.rsp.travelandwork.service;
 
+import cz.cvut.fel.rsp.travelandwork.dao.TripSessionDao;
+import cz.cvut.fel.rsp.travelandwork.model.TripSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 
 @Service
 public class TripSessionService {
+
+    private TripSessionDao tripSessionDao;
+
+    @Autowired
+    public TripSessionService(TripSessionDao tripSessionDao) {
+        this.tripSessionDao = tripSessionDao;
+    }
+
+    @Transactional
+    public List<TripSession> findAllInTrip(String trip_short_name) {
+        return tripSessionDao.find(trip_short_name);
+    }
 }
