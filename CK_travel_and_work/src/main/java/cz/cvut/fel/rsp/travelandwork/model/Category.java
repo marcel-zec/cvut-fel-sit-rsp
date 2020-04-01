2d42d679ffc7c63b21cd01d7ed93513d694b2ea3
Marcel Žec <zecmarce@fel.cvut.cz>
2020-04-01 13:55:06 +0200

    category - constructor, getter and setter, json ignore

--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/model/Category.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/model/Category.java
@@ -1,7 +1,10 @@
 package cz.cvut.fel.rsp.travelandwork.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -9,9 +12,24 @@ import java.util.List;
 public class Category extends AbstractEntity{
 
     @Basic(optional = false)
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 30, unique = true)
     @NotBlank(message = "Name of category cannot be blank")
     private String name;
+
+    public Category(@NotBlank(message = "Name of category cannot be blank") String name) {
+        this.name = name;
+    }
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Trip> trips;
+
+    public boolean add(Trip trip){
+        return trips.add(trip);
+    }
+
+    public Category() {
+    }
 
     public String getName() {
         return name;
@@ -21,15 +39,15 @@ public class Category extends AbstractEntity{
         this.name = name;
     }
 
-    public Category(@NotBlank(message = "Name of category cannot be blank") String name) {
-        this.name = name;
+    public List<Trip> getTrips() {
+        return trips;
     }
 
-    @OneToMany(mappedBy = "category")
-    private List<Trip> trips;
-
-    public boolean add(Trip trip){
-        return trips.add(trip);
+    public void addTrips(Trip trip) {
+        if (this.trips == null) this.trips = new ArrayList<Trip>();
+        if (!this.trips.contains(trip)){
+            this.trips.add(trip);
+        }
     }
 }
 
