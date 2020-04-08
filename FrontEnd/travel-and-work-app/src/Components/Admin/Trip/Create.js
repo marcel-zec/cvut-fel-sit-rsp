81e9ad17ad15bebd52d1c935cb70521b33a0d839
Marcel Žec <zecmarce@fel.cvut.cz>
2020-04-08 17:41:17 +0200

    refactor create trip form

--- FrontEnd/travel-and-work-app/src/Components/Admin/Trip/Create.js
+++ FrontEnd/travel-and-work-app/src/Components/Admin/Trip/Create.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import { Col, Button, Row } from "react-bootstrap";
+import { Col, Button, Row, Spinner } from "react-bootstrap";
 import { Container } from "react-bootstrap";
-import AchievementFormGroup from "./UI/AchievementFormGroup";
+import Achievements from "./UI/Achievements";
 import SessionGroup from "./SessionGroup";
 
 class Create extends React.Component {
     state = {
         achievements: null,
-        show: true,
         categories: null,
         form: {
             isValid: false,
@@ -164,19 +163,19 @@ class Create extends React.Component {
         console.log(this.state);
     };
 
-    fetchAchievementsHandler = async () => {
-        const response = await fetch(`http://localhost:8080/achievement`);
-        const data = await response.json();
-        console.log(data);
-        //show: false -> add class name to button and hide it
-        this.setState({ achievements: data, show: false });
-    };
+    fetchAchievementsHandler = async () => {};
 
     async componentDidMount() {
-        const response = await fetch(`http://localhost:8080/category`);
-        const data = await response.json();
-        console.log(data);
-        this.setState({ categories: data });
+        const response1 = await fetch(`http://localhost:8080/category`);
+        const data1 = await response1.json();
+        console.log(data1);
+        this.setState({ categories: data1 });
+
+        const response2 = await fetch(`http://localhost:8080/achievement`);
+        const data2 = await response2.json();
+        console.log(data2);
+        //show: false -> add class name to button and hide it
+        this.setState({ achievements: data2 });
     }
 
     submitHandler = (event) => {
@@ -185,13 +184,21 @@ class Create extends React.Component {
     };
 
     render() {
-        let possibleXPrewardOptions = [];
-        for (let i = 0; i < 25; i++) {
-            possibleXPrewardOptions.push(<option>{i + 1}</option>);
-        }
+        if (this.state.achievements == null && this.state.categories == null) {
+            return (
+                <Container className="p-5 mt-5">
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                </Container>
+            );
+        } else {
+            let possibleXPrewardOptions = [];
+            for (let i = 0; i < 25; i++) {
+                possibleXPrewardOptions.push(<option>{i + 1}</option>);
+            }
 
-        let categoryOptions = null;
-        if (this.state.categories !== null) {
+            let categoryOptions = null;
             if (this.state.categories.length > 0) {
                 let categoriesArray = [];
 
@@ -209,163 +216,126 @@ class Create extends React.Component {
                     </Form.Control>
                 );
             }
-        }
 
-        let requiredAchievements = null;
-        let gainAchievements = null;
+            return (
+                <Container>
+                    <Form className="mt-3 mb-5" onSubmit={this.submitHandler}>
+                        <h1>Create trip</h1>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridName">
+                                <Form.Label>Name of trip</Form.Label>
+                                <Form.Control
+                                    placeholder="Enter name"
+                                    onChange={(event) =>
+                                        this.inputUpdateHandler(event, "name")
+                                    }
+                                />
+                            </Form.Group>
 
-        if (this.state.achievements !== null) {
-            if (this.state.achievements.length > 0) {
-                requiredAchievements = (
-                    <AchievementFormGroup
-                        label="Required achievements"
-                        items={this.state.achievements}
-                        formInputName="requeired_achievements"
-                        onChangeMethod={(event) =>
-                            this.inputUpdateHandler(
-                                event,
-                                "required_achievements"
-                            )
-                        }
-                    />
-                );
-                gainAchievements = (
-                    <AchievementFormGroup
-                        label="Gain achievements"
-                        items={this.state.achievements}
-                        formInputName="gain_achievements"
-                        onChangeMethod={(event) =>
-                            this.inputUpdateHandler(event, "gain_achievements")
-                        }
-                    />
-                );
-            }
-        }
+                            <Form.Group as={Col} controlId="formGridShortName">
+                                <Form.Label>Identificatation name</Form.Label>
+                                <Form.Control
+                                    placeholder="Enter unique key for trip"
+                                    onChange={(event) =>
+                                        this.inputUpdateHandler(
+                                            event,
+                                            "short_name"
+                                        )
+                                    }
+                                />
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridDeposit">
+                                <Form.Label>Deposit</Form.Label>
+                                <Form.Control
+                                    placeholder="Enter deposite price"
+                                    onChange={(event) =>
+                                        this.inputUpdateHandler(
+                                            event,
+                                            "deposit"
+                                        )
+                                    }
+                                />
+                            </Form.Group>
 
-        let sessions = [];
-        /*       this.state.sessions.forEach(() => {
-            sessions.push(<SessionInput />);
-        });
-*/
-        return (
-            <Container>
-                <Form className="mt-3 mb-5" onSubmit={this.submitHandler}>
-                    <h1>Create trip</h1>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="formGridName">
-                            <Form.Label>Name of trip</Form.Label>
-                            <Form.Control
-                                placeholder="Enter name"
-                                onChange={(event) =>
-                                    this.inputUpdateHandler(event, "name")
-                                }
-                            />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridShortName">
-                            <Form.Label>Identificatation name</Form.Label>
-                            <Form.Control
-                                placeholder="Enter unique key for trip"
-                                onChange={(event) =>
-                                    this.inputUpdateHandler(event, "short_name")
-                                }
-                            />
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="formGridDeposit">
-                            <Form.Label>Deposit</Form.Label>
-                            <Form.Control
-                                placeholder="Enter deposite price"
-                                onChange={(event) =>
-                                    this.inputUpdateHandler(event, "deposit")
-                                }
-                            />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridExperience">
-                            <Form.Label>Required level</Form.Label>
-                            <Form.Control
-                                placeholder="Enter minimum reqiured level"
-                                onChange={(event) =>
-                                    this.inputUpdateHandler(
-                                        event,
-                                        "required_level"
-                                    )
-                                }
-                            />
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group
-                            as={Col}
-                            controlId="exampleForm.ControlSelect1"
-                        >
-                            <Form.Label>Possible XP reward</Form.Label>
-                            <Form.Control
-                                as="select"
-                                onChange={(event) =>
-                                    this.inputUpdateHandler(
-                                        event,
-                                        "possible_xp_reward"
-                                    )
-                                }
+                            <Form.Group as={Col} controlId="formGridExperience">
+                                <Form.Label>Required level</Form.Label>
+                                <Form.Control
+                                    placeholder="Enter minimum reqiured level"
+                                    onChange={(event) =>
+                                        this.inputUpdateHandler(
+                                            event,
+                                            "required_level"
+                                        )
+                                    }
+                                />
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group
+                                as={Col}
+                                controlId="exampleForm.ControlSelect1"
                             >
-                                {possibleXPrewardOptions}
-                            </Form.Control>
+                                <Form.Label>Possible XP reward</Form.Label>
+                                <Form.Control
+                                    as="select"
+                                    onChange={(event) =>
+                                        this.inputUpdateHandler(
+                                            event,
+                                            "possible_xp_reward"
+                                        )
+                                    }
+                                >
+                                    {possibleXPrewardOptions}
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group
+                                as={Col}
+                                controlId="exampleForm.ControlSelect1"
+                            >
+                                <Form.Label>Category</Form.Label>
+                                {categoryOptions}
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Group controlId="formGridLocation">
+                            <Form.Label>Location</Form.Label>
+                            <Form.Control
+                                placeholder="Hotel Super, London Street 12, Manchester, England"
+                                onChange={(event) =>
+                                    this.inputUpdateHandler(event, "location")
+                                }
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows="5"
+                                onChange={(event) =>
+                                    this.inputUpdateHandler(
+                                        event,
+                                        "description"
+                                    )
+                                }
+                            />
                         </Form.Group>
 
-                        <Form.Group
-                            as={Col}
-                            controlId="exampleForm.ControlSelect1"
-                        >
-                            <Form.Label>Category</Form.Label>
-                            {categoryOptions}
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Group controlId="formGridLocation">
-                        <Form.Label>Location</Form.Label>
-                        <Form.Control
-                            placeholder="Hotel Super, London Street 12, Manchester, England"
-                            onChange={(event) =>
-                                this.inputUpdateHandler(event, "location")
-                            }
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control
-                            as="textarea"
-                            rows="5"
-                            onChange={(event) =>
-                                this.inputUpdateHandler(event, "description")
-                            }
-                        />
-                    </Form.Group>
-                    <SessionGroup
-                        onChangeMethod={this.inputSessionUpdateHandler}
-                        sessions={this.state.trip.sessions}
-                        forDeleteSession={this.sessionDeleteHandler}
-                    />
-                    <Button
-                        variant="primary"
-                        type="button"
-                        onClick={() => this.fetchAchievementsHandler()}
-                        className={this.state.show ? "" : "d-none"}
-                    >
-                        Achievements
-                    </Button>
-                    <Form.Row>
-                        {requiredAchievements}
+                        <Achievements items={this.state.achievements} />
 
-                        {gainAchievements}
-                    </Form.Row>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </Container>
-        );
+                        <SessionGroup
+                            onChangeMethod={this.inputSessionUpdateHandler}
+                            sessions={this.state.trip.sessions}
+                            forDeleteSession={this.sessionDeleteHandler}
+                        />
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Container>
+            );
+        }
     }
 }
 
