d23f7e66f61102300cb960a37c6fc78f1ab0d39f
Sandra Hamráková <hamrasan@fel.cvut.cz>
2020-04-08 19:23:32 +0200

    exception pridanie vynimiek do tripservice

---
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/exception/MissingVariableException.java
@@ -0,0 +1,8 @@
+package cz.cvut.fel.rsp.travelandwork.exception;
+
+public class MissingVariableException extends Exception {
+    public MissingVariableException() {
+
+        super("missing variable");
+    }
+}
