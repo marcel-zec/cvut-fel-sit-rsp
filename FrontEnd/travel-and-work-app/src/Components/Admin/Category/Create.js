d6ffbefd0f39a5ca0361a10fd617c3d801d1f3ff
Marcel Žec <zecmarce@fel.cvut.cz>
2020-04-11 16:30:41 +0200

    category validation

--- FrontEnd/travel-and-work-app/src/Components/Admin/Category/Create.js
+++ FrontEnd/travel-and-work-app/src/Components/Admin/Category/Create.js
@@ -3,6 +3,12 @@ import Form from "react-bootstrap/Form";
 import { Col, Button, Row } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import ButtonInRow from "../../SmartGadgets/ButtonInRow";
+import rules from "../../../Files/validationRules.json";
+import {
+    formValidation,
+    validationFeedback,
+    validationClassName,
+} from "../../../Validator";
 
 class Create extends React.Component {
     state = {
@@ -13,20 +19,15 @@ class Create extends React.Component {
                 name: {
                     touched: false,
                     valid: false,
-                    validationRules: {
-                        required: true,
-                        minLength: 3,
-                        maxLength: 5,
-                    },
+                    validationRules: rules.category.name,
                 },
             },
         },
     };
 
-    submitHandler = (event) => {
+    submitHandler = async (event) => {
         event.preventDefault();
-        console.log(this.state.category);
-        this.validateForm();
+        await this.validateForm();
         if (this.state.form.isValid) {
             fetch("http://localhost:8080/category", {
                 method: "POST",
@@ -48,9 +49,13 @@ class Create extends React.Component {
      * @param {event} event
      * @param {String} nameOfFormInput,
      */
-    inputUpdateHandler = async (event, nameOfFormInput) => {
+    inputUpdateHandler = async (event, nameOfFormInput, number = false) => {
         const newState = { ...this.state.category };
-        newState[nameOfFormInput] = event.target.value;
+        let input = event.target.value;
+        if (number) {
+            input = event.target.value.replace(/,/g, ".");
+        }
+        newState[nameOfFormInput] = input;
         await this.setState({ category: newState });
         if (this.state.form.elements[nameOfFormInput].touched) {
             this.validateForm();
@@ -58,107 +63,9 @@ class Create extends React.Component {
     };
 
     validateForm = async () => {
-        console.log("in validation");
         const newState = { ...this.state.form };
-        for (let inputName in this.state.form.elements) {
-            const input = this.state.category[inputName];
-            const rules = newState.elements[inputName].validationRules;
-            newState.elements[inputName].touched = true;
-            newState.elements[inputName].valid = true;
-            newState.valid = true;
-            if (rules.hasOwnProperty("required") && rules.required) {
-                if (!input || input.trim() === "") {
-                    newState.elements[inputName].valid = false;
-                    newState.valid = false;
-                    console.log("required check: INVALID");
-                } else {
-                    console.log("required check:  valid");
-                }
-            }
-            if (rules.hasOwnProperty("minLength")) {
-                if (!input || input.trim().length < rules.minLength) {
-                    newState.elements[inputName].valid = false;
-                    newState.valid = false;
-                    console.log(
-                        "min length (" + rules.minLength + ") check: INVALID"
-                    );
-                } else {
-                    console.log(
-                        "min length (" + rules.minLength + ") check: valid"
-                    );
-                }
-            }
-            if (rules.hasOwnProperty("maxLength")) {
-                if (!input || input.trim().length > rules.maxLength) {
-                    newState.elements[inputName].valid = false;
-                    newState.valid = false;
-                    console.log(
-                        "max length (" + rules.maxLength + ") check: INVALID"
-                    );
-                } else {
-                    console.log(
-                        "max length (" + rules.maxLength + ") check: valid"
-                    );
-                }
-            }
-            newState.isValid = newState.elements[inputName].valid;
-        }
-        console.log("Form valid: " + newState.isValid);
+        formValidation(newState, this.state.category);
         await this.setState({ form: newState });
-        console.log("Form valid after setState: " + this.state.form.isValid);
-    };
-
-    /**
-     * Return class name for form input after validation(touched is true).
-     */
-    validationClassName = (inputName) => {
-        const elements = this.state.form.elements;
-        if (elements[inputName].touched) {
-            if (elements[inputName].valid) return "is-valid";
-            return "is-invalid";
-        } else {
-            return "";
-        }
-    };
-
-    /**
-     * Set text of validation feedback by rules of input.
-     */
-    validationFeedback = (inputName) => {
-        const rules = this.state.form.elements[inputName].validationRules;
-        let text = "";
-        if (rules.hasOwnProperty("required") && rules.required) {
-            text += this.capitalizeFirstLetter(inputName) + " is required. ";
-        }
-        if (rules.hasOwnProperty("minLength")) {
-            if (text.length > 0) {
-                text += "Minimal length is " + rules.minLength + ". ";
-            } else {
-                text +=
-                    "Minimal length of " +
-                    this.capitalizeFirstLetter(inputName) +
-                    " is " +
-                    rules.minLength +
-                    ". ";
-            }
-        }
-        if (rules.hasOwnProperty("maxLength")) {
-            if (text.length > 0) {
-                text += "Maximal length is " + rules.maxLength + ". ";
-            } else {
-                text +=
-                    "Maximal length of " +
-                    this.capitalizeFirstLetter(inputName) +
-                    " is " +
-                    rules.maxLength +
-                    ". ";
-            }
-        }
-        return text;
-    };
-
-    capitalizeFirstLetter = (string) => {
-        return string.charAt(0).toUpperCase() + string.slice(1);
     };
 
     render() {
@@ -178,14 +85,17 @@ class Create extends React.Component {
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label>Name of category</Form.Label>
                         <Form.Control
-                            className={this.validationClassName("name")}
+                            className={validationClassName(
+                                "name",
+                                this.state.form
+                            )}
                             placeholder="Enter name"
                             onChange={(event) =>
-                                this.inputUpdateHandler(event, "name")
+                                this.inputUpdateHandler(event, "name", true)
                             }
                         />
                         <div class="invalid-feedback">
-                            {this.validationFeedback("name")}
+                            {validationFeedback("name", this.state.form)}
                         </div>
                     </Form.Group>
 
