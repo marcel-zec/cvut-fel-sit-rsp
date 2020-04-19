f32c1f62de8a56c69d0bf90c81d1835e7cd1f92a
Marcel Žec <zecmarce@fel.cvut.cz>
2020-04-19 15:30:13 +0200

    login form validation

--- FrontEnd/travel-and-work-app/src/Components/Auth/Login.js
+++ FrontEnd/travel-and-work-app/src/Components/Auth/Login.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import { Container,Col, Button, Row, Spinner } from "react-bootstrap";
-
+import { Container, Col, Button, Row, Spinner } from "react-bootstrap";
+import rules from "../../Files/validationRules.json";
+import {
+    formValidation,
+    validationFeedback,
+    validationClassName,
+} from "../../Validator";
 class Login extends React.Component {
     state = {
         form: {
@@ -10,53 +15,90 @@ class Login extends React.Component {
                 email: {
                     touched: false,
                     valid: false,
-                    validationRules: [],
+                    validationRules: rules.login.email,
                 },
                 password: {
                     touched: false,
                     valid: false,
-                    validationRules: [],
+                    validationRules: rules.login.password,
                 },
-                checkbox: {
-                    touched: false,
-                    valid: false,
-                    validationRules: [],
-                }
-            }
+            },
         },
         user: {
             email: null,
             password: null,
-            checkbox: false
-        }
+        },
     };
-    inputUpdateHandler = (event, nameOfFormInput) => {
-        const stringProperties = [
-            "email",
-            "password"
-        ];
+
+    inputUpdateHandler = async (event, nameOfFormInput) => {
+        const stringProperties = ["email", "password"];
         let newState = { ...this.state.user };
         //string inputs
         if (stringProperties.includes(nameOfFormInput)) {
             newState[nameOfFormInput] = event.target.value;
-        } 
-        this.setState({ user: newState });
+        }
+        await this.setState({ user: newState });
+        if (
+            this.state.form.elements.hasOwnProperty(nameOfFormInput) &&
+            this.state.form.elements[nameOfFormInput].touched
+        ) {
+            this.validateForm();
+        }
         console.log(this.state.user);
     };
-    submitHandler = (event) => {
+
+    submitHandler = async (event) => {
         event.preventDefault();
-        let checkbox = document.getElementById("checkbox_check");
-        this.state.user.checkbox = checkbox.checked;
         console.log(this.state.user);
+        await this.validateForm();
+        if (this.state.form.isValid) {
+            fetch("http://localhost:8080/login", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(this.state.user),
+            }).then((response) => {
+                // if (response.ok) this.props.history.push("/trip");
+                //TODO - osetrenie vynimiek
+                //  else console.log("Error: somethhing goes wrong");
+                console.log(response);
+            });
+        }
     };
+
+    validateForm = async () => {
+        console.log("in validation");
+        const newState = { ...this.state.form };
+        formValidation(newState, this.state.user);
+        await this.setState({ form: newState });
+    };
+
     render() {
         return (
             <Container className="login_container">
                 <Row>
-                    <Form className="window radius login_form" onSubmit={this.submitHandler}>
+                    <Form
+                        className="window radius login_form"
+                        onSubmit={this.submitHandler}
+                    >
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" onChange={(event) => this.inputUpdateHandler(event, "email")} required/>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                onChange={(event) =>
+                                    this.inputUpdateHandler(event, "email")
+                                }
+                                className={validationClassName(
+                                    "email",
+                                    this.state.form
+                                )}
+                            />
+                            <div class="invalid-feedback">
+                                {validationFeedback("email", this.state.form)}
+                            </div>
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
@@ -64,12 +106,29 @@ class Login extends React.Component {
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" onChange={(event) => this.inputUpdateHandler(event, "password")} required />
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                onChange={(event) =>
+                                    this.inputUpdateHandler(event, "password")
+                                }
+                                className={validationClassName(
+                                    "password",
+                                    this.state.form
+                                )}
+                            />
+                            <div class="invalid-feedback">
+                                {validationFeedback(
+                                    "password",
+                                    this.state.form
+                                )}
+                            </div>
                         </Form.Group>
-                        <Form.Group controlId="formBasicCheckbox">
-                            <label className="checkbox">Check me out <Form.Check type="checkbox" id="checkbox_check" /></label>
-                        </Form.Group>
-                        <Button variant="primary" type="submit" className="submit">
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            className="submit"
+                        >
                             Login
                         </Button>
                     </Form>
