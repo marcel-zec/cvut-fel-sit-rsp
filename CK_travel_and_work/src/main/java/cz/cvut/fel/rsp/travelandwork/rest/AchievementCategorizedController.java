ae38cee1c0fc904d87a288e6a3762e01675dffb2
sakacond-eshop.git <sakacond@fel.cvut.cz>
2020-04-22 13:28:26 +0200

    filled up 3 achievement controllers

--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/rest/AchievementCategorizedController.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/rest/AchievementCategorizedController.java
@@ -1,12 +1,16 @@
 package cz.cvut.fel.rsp.travelandwork.rest;
 
 import cz.cvut.fel.rsp.travelandwork.model.AchievementCategorized;
+import cz.cvut.fel.rsp.travelandwork.model.AchievementCertificate;
 import cz.cvut.fel.rsp.travelandwork.service.AchievementCategorizedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -14,4 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class AchievementCategorizedController {
     private static final Logger LOG = LoggerFactory.getLogger(AchievementCertificateController.class);
     private final AchievementCategorizedService achievementCategorizedService;
+
+    @Autowired
+    public AchievementCategorizedController(AchievementCategorizedService achievementCategorizedService) {
+        this.achievementCategorizedService = achievementCategorizedService;
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public AchievementCategorized get(@PathVariable Long id){
+        return achievementCategorizedService.find(id);
+    }
+
+    @PatchMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody AchievementCategorized achievement){
+        achievementCategorizedService.update(achievement);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<AchievementCategorized> getAll() {
+        return achievementCategorizedService.findAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody AchievementCategorized achievement){
+        achievementCategorizedService.create(achievement);
+    }
 }
