1b2f674ca75fb0ab89007097cc14a2dc9b363cf8
Sandra Hamráková <hamrasan@fel.cvut.cz>
2020-05-02 11:59:43 +0200

    nullable prec v trip review popis create nedokonceny v trip review

--- CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/service/TripReviewService.java
+++ CK_travel_and_work/src/main/java/cz/cvut/fel/rsp/travelandwork/service/TripReviewService.java
@@ -1,8 +1,11 @@
 package cz.cvut.fel.rsp.travelandwork.service;
 
 import cz.cvut.fel.rsp.travelandwork.dao.TripReviewDao;
+import cz.cvut.fel.rsp.travelandwork.dao.UserDao;
+import cz.cvut.fel.rsp.travelandwork.exception.UnauthorizedException;
 import cz.cvut.fel.rsp.travelandwork.model.TripReview;
-import org.springframework.stereotype.Repository;
+import cz.cvut.fel.rsp.travelandwork.model.User;
+import cz.cvut.fel.rsp.travelandwork.security.SecurityUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +16,11 @@ import java.util.Objects;
 public class TripReviewService {
 
     private final TripReviewDao tripReviewDao;
+    private final UserDao userDao;
 
-    public TripReviewService(TripReviewDao tripReviewDao) {
+    public TripReviewService(TripReviewDao tripReviewDao, UserDao userDao) {
         this.tripReviewDao = tripReviewDao;
+        this.userDao = userDao;
     }
 
     @Transactional
@@ -29,7 +34,11 @@ public class TripReviewService {
     }
 
     @Transactional
-    public void persist(TripReview tripReview) {
+    public void create(TripReview tripReview, User author) throws UnauthorizedException {
+        Objects.requireNonNull(tripReview);
+        if (SecurityUtils.isAuthenticatedAnonymously()) throw new UnauthorizedException();
+        tripReview.setAuthor(userDao.find(SecurityUtils.getCurrentUser().getId()));
+        //priradit trip
         tripReviewDao.persist(tripReview);
     }
 
